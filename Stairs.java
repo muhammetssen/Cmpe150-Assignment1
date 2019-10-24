6274e699eeb574402bfe3cc547478b7dff101788
Muhammet <enmuhammet23@gmail.com>
2019-10-24 22:30:51 +0300

    4 for loops and 4 methods

--- Stairs.java
+++ Stairs.java
@@ -2,78 +2,72 @@ public class Stairs {
 	public static void main(String[] args) {
 		//int stickmanHeight = Integer.parseInt(args[0]);
 		//int stairHeight = Integer.parseInt(args[1]);
-		int stickmanHeight = 10, stairHeight = 6;
-		stairs(stairHeight,stickmanHeight);
-	}
-	
-	
-	public static void stairs(int stairHeight,int stickmanHeight) {
-		System.out.print(multiplier(stairHeight, "\n"));
+		int stickmanHeight = 12, stairHeight = 10;	
+		multiplier(stairHeight, "\n");
 		String head_shape = " O ", torso_shape = "/|\\ ",body_shape = " | ", leg_shape = "/ \\";
-		int current_level = stairHeight; // Top stair's index is 0. At the beginning, stickman is at the bottom whose index equals to stairHeight
+		String space_on_left = "";
 		for (int level = 0; level <=stairHeight; level++) { // Top To Bottom. Printing every stair of a frame.
-			String space_on_left = "";
-			space_on_left = multiplier(stairHeight-current_level, "   ");
-			
-			// Stickman's height and torso are always higher than the stairs. We don't need to adjust these according to variables. 
 			System.out.println(space_on_left + head_shape);
 			System.out.println(space_on_left + torso_shape);
-			
-			/* I will use 3 separate for loops. 
-			   First situation is that there are no stairs at the right of the '|' part. Printing spaces and ' I ' is enough.
-			   Second situation is that there are both a body part, underscores and stars in a line. Printing order is spaces, " | ", underscores, stars and '|'.
-			   Third situation is that stickman has already climbed those stairs so printing only the stair structure is enough. There is no need to evaluate stickman's position. */
-			   
 
-			//First Situation - Printing body only
-			int body_parts = stickmanHeight - 3; // Number of the '|' parts .   Total Height - 1(for head) - 1(for torso) - 1(for legs). 
-			int above_stairs = body_parts-current_level; // How many of '|' parts satisfy the condition for the first situation
-			int printed_body_parts = 0; // Number of  '|' parts I have printed
-			System.out.print(multiplier(above_stairs, space_on_left + body_shape + "\n"));
-			printed_body_parts += above_stairs;
-			
-			
-			/* Printing a body part and a stair at the same line*/
-			int remained_body_parts = body_parts - printed_body_parts;
-			int drawing_step = 0; //Bottom one = 2
-			for (int i = 0; i < remained_body_parts; i++) { // Each loop is a new line
-				System.out.print(space_on_left + body_shape);
-				System.out.print(multiplier(current_level-drawing_step,"   "));
-				star_printer(drawing_step);
-				drawing_step++;
-			}
-			
-			/*Printing legs and a step*/
+			multiplier(stickmanHeight-3-(stairHeight-level), space_on_left + body_shape + "\n");	
+
+			line(stairHeight,level, space_on_left+" | ",0);
+
 			System.out.print(space_on_left + leg_shape);
-			System.out.print(multiplier(current_level - drawing_step, "   "));
-			star_printer(drawing_step);
-			drawing_step++;
-			
-			
-			
-			/* Printing without any body or leg*/
-			for (; drawing_step <=stairHeight ; drawing_step++) { //Each loop is a new line
-			System.out.print(multiplier(stairHeight - drawing_step + 1, "   "));
-			star_printer(drawing_step);
-			}
-			
-			System.out.print(multiplier(current_level + 2, "\n"));
-			current_level--;
-			
+			star_printer(stairHeight-level);
+
+			bottom(stairHeight, level-1,"");
+			//System.out.println("------------------");
+			//line(level+stairHeight,stairHeight,"",stairHeight-level+1); // first - second = level+1
+
+			multiplier(stairHeight-level + 2, "\n");
+			space_on_left +="   ";
 		} 
 	}
+		
+	public static void line( int stairHeight,int level,String part ,int space) {
+		for (int i = 0; i < stairHeight-level; i++) {  // stair-level times
+			//System.out.println("j "+i);
+			//System.out.println("line : stair "+stairHeight + " level "+level+" i "+i);
+
+			System.out.print(part);
+			//System.out.println(stairHeight-i+1 + "spaces and " +i+"stars" );
+
+			multiplier(stairHeight-level-i,"   ");
+			star_printer(i+space);
+		}	
+	}
 	
-	public static void star_printer(int count) {
-		System.out.print("___|");
-		System.out.print(multiplier(count, "***"));
-		System.out.println('|');
+	public static void bottom (int stairHeight, int level,String part) {		
+		for (int i = stairHeight-level; i <=stairHeight ; i++) { // level + 1 times iterates
+			//System.out.println("i "+i);
+			//System.out.println("bottom : stair "+stairHeight + " level "+level+" i "+i);
+			System.out.print(part);
+			//System.out.println(stairHeight-i+1 + "spaces and " +i+"stars");
+			multiplier(stairHeight - i + 1, "   "); //level+1,level
+			star_printer(i);
+		}	
+		/*for (int i = 0; i < stairHeight-level; i++) {
+			System.out.print(part);
+			System.out.println(stairHeight-i+1 + "spaces and " +i+"stars" );
+			multiplier(stairHeight-level-i,"   ");
+			star_printer(i);
+			
+		}*/
+		
 	}
 
+	public static void star_printer(int count) {
+		System.out.print("___|");
+		multiplier(count, "***");
+		System.out.println('|');
+	}
 	
-	public static String multiplier(int count, String text){
+	public static void multiplier(int count, String text){
 		String result = "";
 		for (; count >0 ; count --) 
 			result += text;
-		return result;
+		System.out.print(result);;
 	}
 }
